724b8897415410ed695a59ffd6a355a4993fd325
Aman <meamanx7@gmail.com>
2026-04-01 22:20:32 +0530

    Added query middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send("hello this is ur response");
+/* 
+in header we'll need user age and password , in body we'll need user's secret question and ans
+and it query we'll need user name and birthplace  - phase 1 
+ 
+
+ make user regiestration and then authenticate from the stored data - phase 2
+*/  
+
+
+const authMiddle = (req,res,next) =>{
+    const name = req.query.name;
+    const birthplace = req.query.birthplace;
+    if(name === 'Aman' && birthplace === 'Jamui'){
+        next();
+    }else{
+        res.status(401).json({
+            "msg" : "Sorry wrong info"
+        });
+    }
+}
+
+
+app.post('/authenticate', authMiddle, (req,res) => {
+
+    
+    const secretQsn = req.body.secretQsn;
+    const secretAns = req.body.secretAns;
+    const age = req.headers.age;
+    const password = req.headers.password;
+
+
 });
 
 
